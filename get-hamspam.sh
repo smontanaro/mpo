b0855007dc0f811632e658c8e883f9517fc8aac7
Skip Montanaro <skip.montanaro@gmail.com>
2018-12-22 17:32:14 -0600

    pull cull files and tte.log, don't auto-start vm

--- get-hamspam.sh
+++ get-hamspam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Pull spam and ham mailboxes from mpo...
+# Pull spam and ham mailboxes from mpo...  We copy the cull versions, as
+# they are the output of the last training effort and will be the start of
+# the next.  Also pull the latest log file for debugging.
 ssh mail.python.org \
-    sudo bash -c '"cd /usr/local/spambayes-corpus ; tar cfj - {spam,ham}.mbox"' \
+    sudo bash -c '"cd /usr/local/spambayes-corpus ; tar cfj - {spam,ham}.mbox.cull tte.log"' \
     | (cd $(dirname $0) ; tar xvfj -)
-
-emacs --eval '(vm-visit-folder "ham.mbox")'
