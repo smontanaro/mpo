6d9b61e1cb827df825105f55d38bdb0e87809139
Skip Montanaro <skip.montanaro@gmail.com>
2018-12-22 17:32:40 -0600

    work with cull versions

--- put-hamspam.sh
+++ put-hamspam.sh
@@ -4,17 +4,11 @@ cd $(dirname $0)
 
 # Push spam and ham mailboxes to mpo...
 
+# The .cull versions of the mailboxes are the output of the training script,
+# and are thus the starting point for the next cycle.
 # Clean off the VM message cruft.
-bash trim-vm.sh ham.mbox
-bash trim-vm.sh spam.mbox
-
-# Rename the mbox files temporarily, as the .cull versions are the end of
-# the training process. The train script picks up from there and renames
-# them to {ham,spam}.mbox before training proper begins.
-mv spam.mbox spam.mbox.cull
-mv ham.mbox ham.mbox.cull
+bash trim-vm.sh ham.mbox.cull && \
+bash trim-vm.sh spam.mbox.cull && \
 tar cfz - {spam,ham}.mbox.cull \
     | ssh mail.python.org \
           sudo bash -c '"cd /usr/local/spambayes-corpus ; tar xvfz -"'
-mv spam.mbox.cull spam.mbox
-mv ham.mbox.cull ham.mbox
