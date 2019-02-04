f4c51024cee5606e0b232904b802ecc9cb9f11b3
Skip Montanaro <skip.montanaro@gmail.com>
2019-02-03 18:07:56 -0600

    more readable & consider missing file case

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,4 +3,13 @@
 # incorporate new spam/ham messages into corpus and retrain. [sh].mbox are
 # already assumed to have been copied to mpo:/tmp.
 ssh mail.python.org \
-          sudo bash -c '"cd /usr/local/spambayes-corpus ; cat /tmp/h.mbox >> ham.mbox.cull ; mv ham.mbox.cull ham.mbox ; cat /tmp/s.mbox >> spam.mbox.cull  ; mv spam.mbox.cull spam.mbox ; sh train.sh"'
+          sudo bash -c '"cd /usr/local/spambayes-corpus
+if [ -f /tmp/h.mbox ] ; then
+    cat /tmp/h.mbox >> ham.mbox.cull
+fi
+mv ham.mbox.cull ham.mbox
+if [ -f /tmp/s.mbox ] ; then
+    cat /tmp/s.mbox >> spam.mbox.cull
+fi
+mv spam.mbox.cull spam.mbox
+sh train.sh"'
