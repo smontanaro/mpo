3ad656b38ac2041eab51fe53d179812484ee9636
Skip Montanaro <skip.montanaro@gmail.com>
2019-12-30 08:34:57 -0600

    explain use of touch(1)

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,5 +2,10 @@
 
 # Upload latest [sh].mbox files
 
+# Always touch in case our processing didn't write to one file or the
+# other. This guarantees that the mpo version of both files is always
+# new. Otherwise the training script would tend to duplicate recently
+# uploaded messages.
+
 touch s.mbox h.mbox
 scp -C [sh].mbox mail.python.org:/tmp
