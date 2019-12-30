4d21882f3f02de2e68ad60c59c53cb777e1cc9ec
Skip Montanaro <skip.montanaro@gmail.com>
2019-12-30 08:32:50 -0600

    adjust docstring to reflect current reality

--- scripts/dups.py
+++ scripts/dups.py
@@ -2,22 +2,7 @@
 
 """List duplicate message numbers.
 
-Input is the result of this shell pipeline:
-
-cat ham.mbox.cull \\
-| formail -s md5sum \\
-| awk '{printf("%s %010d\n", $0, NR)}'
-
-so input will look something like this:
-
-5c579810d25f20d7117ee6c4cf797e30  - 1
-13b585d039fb5936fa800be14219ed8d  - 2
-ab2eec6316c2b157ca2ef0d0009b3ee0  - 3
-a4743dcb2e0aaa44c0b3dcc91545a044  - 4
-...
-
-For every checksum which is repeated, emit the duplicate message number. A
-full pipeline would look something like this:
+Usage looks something like this:
 
 egrep -i '^message-id:' ham.mbox.cull \\
 | awk '{print $2}' \\
