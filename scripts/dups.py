982d0f79604dd9d262d3c4676c4f128e5f256fda
Skip Montanaro <skip.montanaro@gmail.com>
2021-04-27 20:04:01 -0500

    slight output diff, add -h

--- scripts/dups.py
+++ scripts/dups.py
@@ -5,8 +5,7 @@
 Usage looks something like this:
 
 egrep -i '^message-id:' ham.mbox.cull \\
-| awk '{print $2}' \\
-| awk '{printf("%s %d\n", $2, NR)}'
+| awk '{printf("%s %d\\n", $2, NR)}' \\
 | python dups.py \\
 | sort -r -n
 
@@ -15,17 +14,24 @@ delete from ham.mbox.cull.
 
 """
 
+import getopt
 import sys
 
 def main():
     "see __doc__"
+
+    opts, _args = getopt.getopt(sys.argv[1:], "h")
+    for opt, _arg in opts:
+        if opt == "-h":
+            print(__doc__.strip(), file=sys.stderr)
+            return 0
 
     msgids = set()
 
     for line in sys.stdin:
         msgid, n = line.strip().split()
         if msgid in msgids:
-            print(n)
+            print(n, msgid)
         msgids.add(msgid)
 
 if __name__ == "__main__":
